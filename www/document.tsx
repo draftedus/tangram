c0cddf70735a2ac431d73d48875a42f3f15d8b1f
David Yamnitsky <david@yamnitsky.com>
2020-08-06 16:35:16 -0400

    hi

--- www/document.tsx
+++ www/document.tsx
@@ -1,27 +1,5 @@
 import { Body, DocumentProps, Head, h } from './deps'
 
-let globalCss = `body { background-color: var(--background); color: var(--text); font-family: JetBrains Mono, monospace, sans-serif; margin: 0; line-height: 1; } @font-face { font-family: "JetBrains Mono"; src: url("/jetbrainsmono.woff2") format("woff2"); font-display: swap; } a, button { outline: none } :root {--border-radius: 4px; --border-width: 2px;--max-width: 1200px;--min-width: 256px--border: 2px solid #F2F2F2;--border-color: #F2F2F2;	--blue: #0A84FF;	--gray: #8E8E93;	--green: #30D158;	--indigo: #5e5ce6;	--orange: #FF9F0A;	--pink: #FF375F;	--purple: #BF5AF2;	--red: #FF453A;	--teal: #4DD0E1;	--yellow: #FFD60A;	--background: #FFFFFF; --border: #F2F2F2; --header: #EEEEEE; --hover: #CCCCCC; --surface: #FAFAFA; --muted-text: #666666; --text: #222222; --accent: #0A84FF; --fun-text: #ffffff } @media (prefers-color-scheme: dark) { :root { --background: #111111; --border-color: #212121; --border: 2px solid #212121; --header: #232323; --hover: #333333; --surface: #181818; --muted-text: #8B8B8B; --text: #E8E8E8; --accent: #0A84FF; --fun-text: #111111} }`
-
-// :root {
-// 	scrollbar-width: 4px;
-// 	scrollbar-color: var(--surface) var(--header);
-// }
-
-// ::-webkit-scrollbar {
-// 	width: 4px;
-// }
-
-// ::-webkit-scrollbar-thumb {
-// 	border-radius: 2px;
-// 	background-color: green;
-// }
-
-// ::-webkit-scrollbar-track {
-// 	background-color: var(--header);
-// }
-
-// let driftJs = `"use strict";!function(){var t=window.driftt=window.drift=window.driftt||[];if(!t.init){if(t.invoked)return void(window.console&&console.error&&console.error("Drift snippet included twice."));t.invoked=!0,t.methods=["identify","config","track","reset","debug","show","ping","page","hide","off","on"],t.factory=function(e){return function(){var i=Array.prototype.slice.call(arguments);return i.unshift(e),t.push(i),t}},t.methods.forEach(function(e){t[e]=t.factory(e)}),t.load=function(t){var e=3e5*Math.ceil(new Date/3e5),i=document.createElement("script");i.type="text/javascript",i.async=!0,i.crossorigin="anonymous",i.src="https://js.driftt.com/include/"+e+"/"+t+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(i,n)}}}(),drift.SNIPPET_VERSION="0.3.1",drift.load("iaed56cgc2dc");`
-
 export default function Document(props: DocumentProps) {
 	return (
 		<html lang="en">
@@ -34,15 +12,12 @@ export default function Document(props: DocumentProps) {
 					rel="preload"
 					type="font/woff2"
 				/>
-				<style dangerouslySetInnerHTML={{ __html: globalCss }} />
+				<style src="/global.css" />
 				<title>{'Tangram'}</title>
 				<meta content="Tangram" name="description" />
 			</head>
 			<body>
 				<Body {...props} />
-				{/* {!props.dev && (
-					<script dangerouslySetInnerHTML={{ __html: driftJs }} />
-				)} */}
 			</body>
 		</html>
 	)
