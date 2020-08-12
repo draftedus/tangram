b82d18e128b5f552c08d4b47c0b3843bdd8f9896
David Yamnitsky <david@yamnitsky.com>
2020-08-12 13:53:25 -0400

    hi

--- app/build.rs
+++ app/build.rs
@@ -2,6 +2,6 @@ use std::path::PathBuf;
 
 fn main() {
 	if cfg!(not(debug_assertions)) {
-		pinwheel::build(&PathBuf::from("app"), &PathBuf::from("target/app")).unwrap();
+		pinwheel::build(&PathBuf::from("."), &PathBuf::from("../target/app")).unwrap();
 	}
 }
