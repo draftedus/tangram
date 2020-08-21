ac4157a1c907dfdd20e9856dfc3f03db4c847c12
David Yamnitsky <david@yamnitsky.com>
2020-08-21 16:19:10 -0400

    hi

--- app/pages/repos/new/mod.rs
+++ app/pages/repos/new/mod.rs
@@ -11,12 +11,6 @@ use multer::Multipart;
 use sqlx::prelude::*;
 use tangram_core::id::Id;
 
-struct Options {
-	user: User,
-	form: Option<Form>,
-	error: Option<String>,
-}
-
 #[derive(serde::Serialize)]
 struct Props {
 	error: Option<String>,
@@ -28,13 +22,17 @@ struct Props {
 pub struct Form {
 	pub title: String,
 	pub owner: String,
-	pub file: String,
 }
 
 #[derive(serde::Serialize)]
 struct Owner {
 	value: String,
 	title: String,
+}
+
+enum OwnerType {
+	User,
+	Organization,
 }
 
 pub async fn get(request: Request<Body>, context: &Context) -> Result<Response<Body>> {
@@ -46,27 +44,9 @@ pub async fn get(request: Request<Body>, context: &Context) -> Result<Response<B
 	let user = authorize_user(&request, &mut db)
 		.await?
 		.map_err(|_| Error::Unauthorized)?;
-	let response = render(
-		&mut db,
-		context,
-		Options {
-			user,
-			form: None,
-			error: None,
-		},
-	)
-	.await;
-	db.commit().await?;
-	response
-}
-
-async fn render(
-	db: &mut sqlx::Transaction<'_, sqlx::Any>,
-	context: &Context,
-	options: Options,
-) -> Result<Response<Body>> {
-	let props = props(db, options.user, options.form, options.error).await?;
+	let props = props(&mut db, user, None, None).await?;
 	let html = context.pinwheel.render_with("/repos/new", props)?;
+	db.commit().await?;
 	let response = Response::builder()
 		.status(StatusCode::OK)
 		.body(Body::from(html))
@@ -80,6 +60,10 @@ async fn props(
 	form: Option<Form>,
 	error: Option<String>,
 ) -> Result<Props> {
+	let mut owners = vec![Owner {
+		value: format!("user:{}", user.id),
+		title: user.email,
+	}];
 	let rows = sqlx::query(
 		"
 		select
@@ -94,18 +78,14 @@ async fn props(
 	.bind(&user.id.to_string())
 	.fetch_all(&mut *db)
 	.await?;
-	let mut owners = vec![Owner {
-		value: format!("user:{}", user.id),
-		title: user.email,
-	}];
-	rows.iter().for_each(|row| {
+	for row in rows {
 		let id: String = row.get(0);
 		let title: String = row.get(1);
 		owners.push(Owner {
 			value: format!("organization:{}", id),
 			title,
 		})
-	});
+	}
 	Ok(Props {
 		owners,
 		error,
@@ -122,6 +102,7 @@ pub async fn post(request: Request<Body>, context: &Context) -> Result<Response<
 	let user = authorize_user(&request, &mut db)
 		.await?
 		.map_err(|_| Error::Unauthorized)?;
+
 	let boundary = request
 		.headers()
 		.get(header::CONTENT_TYPE)
@@ -130,7 +111,6 @@ pub async fn post(request: Request<Body>, context: &Context) -> Result<Response<
 		.ok_or(Error::BadRequest)?;
 	let mut title: Option<String> = None;
 	let mut owner: Option<String> = None;
-	let mut file: Option<String> = None;
 	let mut file_data: Option<Vec<u8>> = None;
 	let mut multipart = Multipart::new(request.into_body(), boundary);
 	while let Some(mut field) = multipart.next_field().await? {
@@ -150,7 +130,6 @@ pub async fn post(request: Request<Body>, context: &Context) -> Result<Response<
 				owner = Some(String::from_utf8(field_data).map_err(|_| Error::BadRequest)?);
 			}
 			"file" => {
-				file = Some(field.file_name().ok_or(Error::BadRequest)?.to_string());
 				file_data = Some(field_data);
 			}
 			_ => {}
@@ -158,127 +137,106 @@ pub async fn post(request: Request<Body>, context: &Context) -> Result<Response<
 	}
 	let title = title.ok_or(Error::BadRequest)?;
 	let owner = owner.ok_or(Error::BadRequest)?;
-	let file = file.ok_or(Error::BadRequest)?;
 	let file_data = file_data.ok_or(Error::BadRequest)?;
-	let form = Form { title, owner, file };
+	let form = Form { title, owner };
 
 	let model = match tangram_core::types::Model::from_slice(&file_data) {
 		Ok(model) => model,
 		Err(_) => {
-			return render(
-				&mut db,
-				context,
-				Options {
-					error: Some(String::from("failed to parse model")),
-					form: Some(form),
-					user,
-				},
-			)
-			.await;
+			let error =
+				"The model you uploaded failed to deserialize. Are you sure it is a .tangram file?";
+			let props = props(&mut db, user, Some(form), Some(String::from(error))).await?;
+			let html = context.pinwheel.render_with("/repos/new", props)?;
+			let response = Response::builder()
+				.status(StatusCode::BAD_REQUEST)
+				.body(Body::from(html))
+				.unwrap();
+			return Ok(response);
 		}
 	};
 	let now = Utc::now().timestamp();
 	let repo_id = Id::new();
 
-	let id_parts: Vec<&str> = form.owner.split(':').collect();
-
-	match id_parts[0] {
-		"user" => {
-			let user_id: Id = id_parts[1].parse().map_err(|_| Error::BadRequest)?;
-			if user_id != user.id {
-				return Err(Error::Unauthorized.into());
-			}
-			let result = sqlx::query(
-				"
-					insert into repos (
-						id, created_at, title, user_id
-					) values (
-						?1, ?2, ?3, ?4
-					)
-				",
-			)
-			.bind(&repo_id.to_string())
-			.bind(&now)
-			.bind(&form.title)
-			.bind(&user_id.to_string())
-			.execute(&mut *db)
-			.await;
-			if result.is_err() {
-				return render(
-					&mut db,
-					context,
-					Options {
-						error: Some(String::from("A repo with this title already exists.")),
-						form: Some(form),
-						user,
-					},
-				)
-				.await;
-			}
-		}
-		"organization" => {
-			let organization_id: Id = id_parts[1].parse().map_err(|_| Error::BadRequest)?;
-			if !authorize_user_for_organization(&mut db, &user, organization_id).await? {
-				return Err(Error::Unauthorized.into());
-			}
-			let result = sqlx::query(
-				"
-					insert into repos (
-						id, created_at, title, organization_id
-					) values (
-						?1, ?2, ?3, ?4
-					)
-				",
-			)
-			.bind(&repo_id.to_string())
-			.bind(&now)
-			.bind(&form.title)
-			.bind(&organization_id.to_string())
-			.execute(&mut *db)
-			.await;
-			if result.is_err() {
-				return render(
-					&mut db,
-					context,
-					Options {
-						error: Some(String::from(
-							"A repo in this organization with this title already exists.",
-						)),
-						form: Some(form),
-						user,
-					},
-				)
-				.await;
-			};
-		}
-		&_ => return Err(Error::BadRequest.into()),
+	let mut user_id = None;
+	let mut organization_id = None;
+	let owner_parts: Vec<&str> = form.owner.split(':').collect();
+	let owner_type = owner_parts.get(0).ok_or(Error::BadRequest)?;
+	let owner_type = match owner_type {
+		&"user" => OwnerType::User,
+		&"organization" => OwnerType::Organization,
+		_ => return Err(Error::BadRequest)?,
 	};
+	let owner_id: Id = owner_parts
+		.get(1)
+		.ok_or(Error::BadRequest)?
+		.parse()
+		.map_err(|_| Error::BadRequest)?;
+	match owner_type {
+		OwnerType::User => {
+			if owner_id != user.id {
+				return Err(Error::Unauthorized.into());
+			}
+			user_id = Some(owner_id);
+		}
+		OwnerType::Organization => {
+			if !authorize_user_for_organization(&mut db, &user, owner_id).await? {
+				return Err(Error::Unauthorized.into());
+			}
+			organization_id = Some(owner_id);
+		}
+	};
+	let result = sqlx::query(
+		"
+			insert into repos (
+				id, created_at, title, user_id, organization_id
+			) values (
+				?, ?, ?, ?, ?
+			)
+		",
+	)
+	.bind(&repo_id.to_string())
+	.bind(&now)
+	.bind(&form.title)
+	.bind(&user_id.map(|id| id.to_string()))
+	.bind(&organization_id.map(|id| id.to_string()))
+	.execute(&mut *db)
+	.await;
+	if result.is_err() {
+		let error = "A repo with this title already exists.";
+		let props = props(&mut db, user, Some(form), Some(String::from(error))).await?;
+		let html = context.pinwheel.render_with("/repos/new", props)?;
+		db.commit().await?;
+		let response = Response::builder()
+			.status(StatusCode::BAD_REQUEST)
+			.body(Body::from(html))
+			.unwrap();
+		return Ok(response);
+	}
 
 	let result = sqlx::query(
 		"
 			insert into models
 				(id, repo_id, created_at, data)
-			values
-				(?1, ?2, ?3, ?4)
+			values (
+				?, ?, ?, ?
+			)
 		",
 	)
 	.bind(&model.id().to_string())
 	.bind(&repo_id.to_string())
 	.bind(&now)
-	.bind(&base64::encode(&form.file))
+	.bind(&base64::encode(&file_data))
 	.execute(&mut *db)
 	.await;
 	if result.is_err() {
-		return render(
-			&mut db,
-			context,
-			Options {
-				error: Some(String::from("This model has already been uploaded. Tangram models have unique identifiers and can only belong to one account.")),
-				form: Some(form),
-				user,
-			},
-		)
-		.await;
+		let error = "This model has already been uploaded. Tangram models have unique identifiers and can only belong to one account.";
+		let props = props(&mut db, user, Some(form), Some(String::from(error))).await?;
+		let html = context.pinwheel.render_with("/repos/new", props)?;
+		let response = Response::builder()
+			.status(StatusCode::BAD_REQUEST)
+			.body(Body::from(html))
+			.unwrap();
+		return Ok(response);
 	};
 
 	db.commit().await?;
@@ -286,7 +244,7 @@ pub async fn post(request: Request<Body>, context: &Context) -> Result<Response<
 		.status(StatusCode::SEE_OTHER)
 		.header(
 			header::LOCATION,
-			format!("/repos/{}/models/{}/", repo_id, model.id().to_string()),
+			format!("/repos/{}/models/{}/", repo_id, model.id()),
 		)
 		.body(Body::empty())
 		.unwrap();
