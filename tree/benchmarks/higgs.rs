f0cfc0502a78d12938580384459b7958308390d3
Isabella Tromba <isabella.tromba@gmail.com>
2020-10-16 14:14:27 -0400

    hi

--- tree/benchmarks/higgs.rs
+++ tree/benchmarks/higgs.rs
@@ -10,8 +10,8 @@ fn main() {
 	// Load the data.
 	// let csv_file_path_train = Path::new("data/higgs_500k_train.csv");
 	// let csv_file_path_test = Path::new("data/higgs_500k_test.csv");
-	// let nrows_train = 450_000
-	// let n_rows_test = 50_000;
+	// let _nrows_train = 450_000;
+	// let _n_rows_test = 50_000;
 	let csv_file_path_train = Path::new("data/higgs_train.csv");
 	let csv_file_path_test = Path::new("data/higgs_test.csv");
 	let _nrows_train = 10_500_000;
