dd4119f712d814dee981dac1e3c2c3558d25453b
David Yamnitsky <david@yamnitsky.com>
2020-10-28 12:49:24 -0400

    hi

--- test_npx/src/main.rs
+++ test_npx/src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-	for (key, value) in std::env::vars() {
-		println!("{}: {}", key, value);
-	}
 	let cmd = if cfg!(not(windows)) { "npx" } else { "npx.exe" };
 	let args = vec!["esbuild".to_owned(), "--help".to_owned()];
 	let mut process = std::process::Command::new(dbg!(cmd))
