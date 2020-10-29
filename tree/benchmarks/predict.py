c6f8f4b5fdc78dcde5c964131f782db570d8b147
Isabella Tromba <isabella.tromba@gmail.com>
2020-10-29 16:56:39 -0400

    hi

--- tree/benchmarks/predict.py
+++ tree/benchmarks/predict.py
@@ -1,7 +1,10 @@
 import tangram
 import os
+from time import time
 
+start = time()
 # Get the path to the .tangram file.
-model_path = os.path.join('heart_disease.tangram')
+model_path = os.path.join('data/heart-disease.tangram')
 # Load the model from the file.
 model = tangram.Model.from_file(model_path)
+print(time() - start)
