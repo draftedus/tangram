c356169f63a59df25fec3c20cf7550c469b1199f
Isabella Tromba <isabella.tromba@gmail.com>
2020-10-31 10:10:50 -0400

    hi

--- app/pages/repos/_repo_id/models/_model_id/training_importances/linear_multiclass_classifier.tsx
+++ app/pages/repos/_repo_id/models/_model_id/training_importances/linear_multiclass_classifier.tsx
@@ -9,6 +9,11 @@ export function LinearMulticlassClassifierTrainingImportancesPage(
 	return (
 		<ui.S1>
 			<ui.H1>{'Training Feature Importances'}</ui.H1>
+			<ui.P>
+				{'Your model had a total of '}
+				{props.featureImportances.length}
+				{' features.'}
+			</ui.P>
 			<FeatureImportancesTable featureImportances={props.featureImportances} />
 		</ui.S1>
 	)
