0498eba26d4317f0f1300a271dddd28d8f0e696b
Isabella Tromba <isabella.tromba@gmail.com>
2020-11-08 19:24:45 -0500

    more clean up

--- features/identity.rs
+++ features/identity.rs
@@ -2,7 +2,7 @@ use ndarray::prelude::*;
 use num_traits::ToPrimitive;
 use tangram_dataframe::{
 	DataFrameColumn, DataFrameColumnView, DataFrameValue, EnumDataFrameColumn,
-	NumberDataFrameColumn, TextDataFrameColumn, UnknownDataFrameColumn,
+	EnumDataFrameColumnView, NumberDataFrameColumn, NumberDataFrameColumnView,
 };
 use tangram_util::zip;
 
@@ -53,7 +53,7 @@ pub struct IdentityFeatureGroup {
 impl IdentityFeatureGroup {
 	pub fn compute_array_f32(
 		&self,
-		mut features: ArrayViewMut2<f32>,
+		features: ArrayViewMut2<f32>,
 		column: DataFrameColumnView,
 		progress: &impl Fn(),
 	) {
@@ -61,18 +61,36 @@ impl IdentityFeatureGroup {
 		match column {
 			DataFrameColumnView::Unknown(_) => todo!(),
 			DataFrameColumnView::Number(column) => {
-				for (feature, value) in zip!(features.iter_mut(), column.view().iter()) {
-					*feature = *value;
-					progress()
-				}
+				self.compute_array_f32_for_number_column(features, column, progress)
 			}
 			DataFrameColumnView::Enum(column) => {
-				for (feature, value) in zip!(features.iter_mut(), column.view().iter()) {
-					*feature = value.map(|v| v.get().to_f32().unwrap()).unwrap_or(0.0);
-					progress()
-				}
+				self.compute_array_f32_for_enum_column(features, column, progress)
 			}
 			DataFrameColumnView::Text(_) => todo!(),
+		}
+	}
+
+	fn compute_array_f32_for_number_column(
+		&self,
+		mut features: ArrayViewMut2<f32>,
+		column: NumberDataFrameColumnView,
+		progress: &impl Fn(),
+	) {
+		for (feature, value) in zip!(features.iter_mut(), column.view().iter()) {
+			*feature = *value;
+			progress()
+		}
+	}
+
+	fn compute_array_f32_for_enum_column(
+		&self,
+		mut features: ArrayViewMut2<f32>,
+		column: EnumDataFrameColumnView,
+		progress: &impl Fn(),
+	) {
+		for (feature, value) in zip!(features.iter_mut(), column.view().iter()) {
+			*feature = value.map(|v| v.get().to_f32().unwrap()).unwrap_or(0.0);
+			progress()
 		}
 	}
 
@@ -82,51 +100,79 @@ impl IdentityFeatureGroup {
 		progress: &impl Fn(u64),
 	) -> DataFrameColumn {
 		let column = match column {
-			DataFrameColumnView::Unknown(column) => {
-				let column = UnknownDataFrameColumn::new(column.name().map(|name| name.to_owned()));
-				DataFrameColumn::Unknown(column)
-			}
+			DataFrameColumnView::Unknown(_) => unimplemented!(),
 			DataFrameColumnView::Number(column) => {
-				DataFrameColumn::Number(NumberDataFrameColumn::new(
-					column.name().map(|name| name.to_owned()),
-					column.as_slice().to_owned(),
-				))
+				DataFrameColumn::Number(self.compute_dataframe_for_number_column(column))
 			}
-			DataFrameColumnView::Enum(column) => DataFrameColumn::Enum(EnumDataFrameColumn::new(
-				column.name().map(|name| name.to_owned()),
-				column.options().to_owned(),
-				column.as_slice().to_owned(),
-			)),
-			DataFrameColumnView::Text(column) => DataFrameColumn::Text(TextDataFrameColumn::new(
-				column.name().map(|name| name.to_owned()),
-				column.as_slice().to_owned(),
-			)),
+			DataFrameColumnView::Enum(column) => {
+				DataFrameColumn::Enum(self.compute_datafame_for_enum_column(column))
+			}
+			DataFrameColumnView::Text(_) => unimplemented!(),
 		};
 		progress(column.len().to_u64().unwrap());
 		column
 	}
 
+	fn compute_dataframe_for_number_column(
+		&self,
+		column: NumberDataFrameColumnView,
+	) -> NumberDataFrameColumn {
+		NumberDataFrameColumn::new(
+			column.name().map(|name| name.to_owned()),
+			column.as_slice().to_owned(),
+		)
+	}
+
+	fn compute_datafame_for_enum_column(
+		&self,
+		column: EnumDataFrameColumnView,
+	) -> EnumDataFrameColumn {
+		EnumDataFrameColumn::new(
+			column.name().map(|name| name.to_owned()),
+			column.options().to_owned(),
+			column.as_slice().to_owned(),
+		)
+	}
+
 	pub fn compute_array_value(
 		&self,
-		mut features: ArrayViewMut2<DataFrameValue>,
+		features: ArrayViewMut2<DataFrameValue>,
 		column: DataFrameColumnView,
 		progress: &impl Fn(),
 	) {
 		match column {
 			DataFrameColumnView::Unknown(_) => unimplemented!(),
 			DataFrameColumnView::Number(column) => {
-				for (feature_column, column_value) in zip!(features.column_mut(0), column.iter()) {
-					*feature_column = DataFrameValue::Number(*column_value);
-					progress()
-				}
+				self.compute_array_value_for_number_column(features, column, progress)
 			}
 			DataFrameColumnView::Enum(column) => {
-				for (feature_column, column_value) in zip!(features.column_mut(0), column.iter()) {
-					*feature_column = DataFrameValue::Enum(*column_value);
-					progress()
-				}
+				self.compute_array_value_for_enum_column(features, column, progress)
 			}
 			DataFrameColumnView::Text(_) => unimplemented!(),
+		}
+	}
+
+	fn compute_array_value_for_number_column(
+		&self,
+		mut features: ArrayViewMut2<DataFrameValue>,
+		column: NumberDataFrameColumnView,
+		progress: &impl Fn(),
+	) {
+		for (feature_column, column_value) in zip!(features.column_mut(0), column.iter()) {
+			*feature_column = DataFrameValue::Number(*column_value);
+			progress()
+		}
+	}
+
+	fn compute_array_value_for_enum_column(
+		&self,
+		mut features: ArrayViewMut2<DataFrameValue>,
+		column: EnumDataFrameColumnView,
+		progress: &impl Fn(),
+	) {
+		for (feature_column, column_value) in zip!(features.column_mut(0), column.iter()) {
+			*feature_column = DataFrameValue::Enum(*column_value);
+			progress()
 		}
 	}
 }
