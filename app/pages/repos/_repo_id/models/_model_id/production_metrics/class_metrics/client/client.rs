54357dffad4066d2fad757a595f99b442a0ffe46
Isabella Tromba <isabella.tromba@gmail.com>
2020-12-04 19:32:20 -0500

    hi

--- app/pages/repos/_repo_id/models/_model_id/production_metrics/class_metrics/client/client.rs
+++ app/pages/repos/_repo_id/models/_model_id/production_metrics/class_metrics/client/client.rs
@@ -1,7 +1,12 @@
+use tangram_charts::{hydrate_chart, line_chart::LineChart};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn start() {
 	console_error_panic_hook::set_once();
-	todo!()
+	// TODO: boot date window select
+	// TODO: boot class select
+	hydrate_chart::<LineChart>("precision_intervals");
+	hydrate_chart::<LineChart>("recall_intervals");
+	hydrate_chart::<LineChart>("f1_intervals");
 }
--- app/pages/repos/_repo_id/models/_model_id/production_metrics/index/client/client.rs
+++ app/pages/repos/_repo_id/models/_model_id/production_metrics/index/client/client.rs
@@ -1,7 +1,10 @@
+use tangram_charts::{hydrate_chart, line_chart::LineChart};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn start() {
 	console_error_panic_hook::set_once();
-	todo!()
+	// TODO: boot date window select
+	hydrate_chart::<LineChart>("mse");
+	hydrate_chart::<LineChart>("accuracy");
 }
